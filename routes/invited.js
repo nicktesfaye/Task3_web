b23b69b699bc85add72f575cc640f508046af53d
nicktesfaye <83650404+nicktesfaye@users.noreply.github.com>
2021-08-09 13:45:50 +0530

    Add files via upload

---
+++ routes/invited.js
@@ -0,0 +1,46 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const router = express.Router()
+var jsonParser = bodyParser.json()
+var invite = require('../model/invite')
+
+router.get('/',async(req,res) => {
+
+    //var temp = await data.findById("610eaf609b9b62070ccc4c22")
+    //temp.remove()
+
+    try{
+        const users = await invite.find()
+        res.send(users)
+    } catch(err)
+    {
+        res.send('Error '+err)
+    }
+})
+
+router.post('/',jsonParser,async(req,res) => {
+
+    const user = new invite({
+        to: req.body.email,
+        from:req.session.userEmail
+    })
+    
+
+    try{
+        const a1= await invite.findOne({to: user.to,from:user.from})
+        if(a1==null)
+        {
+        await user.save()
+        res.redirect('/')}
+
+        else{
+            res.redirect('/invite')
+        }
+
+    }catch(err)
+    {
+        res.send('error')
+    }
+})
+
+module.exports =router
