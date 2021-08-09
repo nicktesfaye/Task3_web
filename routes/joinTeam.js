d9cee0cef95857fc7281b055b24400aaf2332061
nicktesfaye <83650404+nicktesfaye@users.noreply.github.com>
2021-08-09 13:42:53 +0530

    Update joinTeam.js

--- routes/joinTeam.js
+++ routes/joinTeam.js
@@ -2,17 +2,38 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const router = express.Router()
 var jsonParser = bodyParser.json()
+var invite =require('../model/invite')
+var data =require('../model/data')
+var invite = require('../model/invite')
 
 router.get('/',async(req,res) => {
-    if(req.session.LogIn==true)
-    res.render('../views/joinTeam.ejs')
+
+    var a1 = invite.findOne({to:req.session.userEmail})
+    var a2 = data.findOne({email:a1.from})
+    if(req.session.LogIn==true){
+        var a1 = await invite.findOne({to:req.session.userEmail})
+        if(a1 == null)
+        res.render('../views/noinvites.ejs')
+        else
+        res.render('../views/joinTeam.ejs',{name :a2.team})
+
+    }
     else
     res.redirect('/login')
 })
 
 router.post('/',jsonParser,async(req,res) => {
-    if(req.session.LogIn==true)
-    res.render('../views/joinTeam.ejs')
+   
+    var a1 = invite.findOne({to:req.session.userEmail})
+    var a2 = data.findOne({email:a1.from})
+    if(req.session.LogIn==true){
+        var a1 = await invite.findOne({to:req.session.userEmail})
+        if(a1 == null)
+        res.render('../views/noinvites.ejs')
+        else
+        res.render('../views/joinTeam.ejs',{name :a2.team})
+
+    }
     else
     res.redirect('/login')
     
