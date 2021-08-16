f670512f2456b66178a8f0008f8fdd89dfeb9eba
nicktesfaye <83650404+nicktesfaye@users.noreply.github.com>
2021-08-16 09:45:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-
+//setup
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const session = require('express-session')
 const url = 'mongodb://localhost/deltaTask3'
 
+//connect to mongodb
 mongoose.connect(url,{useNewUrlParser: true})
 const con = mongoose.connection
 con.on('open',() =>{
     console.log("connected...")
 })
 
+//setup path variables
 const user =require('./routes/user')
 const login_user = require('./routes/login_user')
 const createTeam = require('./routes/createTeam')
@@ -22,7 +24,15 @@ const invite = require('./routes/invite')
 const invited = require('./routes/invited')
 const accept = require('./routes/accept')
 const decline = require('./routes/decline')
+const vote = require('./routes/vote')
+const voted = require('./routes/voted')
+const pollResult = require('./routes/pollResult')
+const endPoll = require('./routes/endPoll')
 
+//other
+const data = require('./model/data')
+
+//set up 
 app.set('view-engine','ejs')
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
@@ -35,10 +45,12 @@ app.use(session({
 app.use(express.static('./views'))
 
 
-
-app.get('/',(req,res)=>{
+//home page
+app.get('/',async(req,res)=>{
+    
     if(req.session.LogIn)
-    res.render('index.ejs')
+    {var a1 = await data.findOne({email:req.session.userEmail})
+    res.render('index.ejs',{team:a1.team})}
     else
     res.redirect('/login')
 })
@@ -50,16 +62,23 @@ app.post('/',(req,res)=>{
     res.redirect('/login')
 })
 
+
+//login page
 app.get('/login',(req,res)=>{
-    req.session.LogIn=false
-res.render('login.ejs')
+    if(req.session.LogIn)
+    res.redirect('/')
+    else
+    res.render('login.ejs')
 })
 
+//register page
 app.get('/register',(req,res)=>{
 res.render('register.ejs')
 })
 
 
+
+//set up routes
 app.use('/users',user)
 app.use('/logData',login_user)
 app.use('/createTeam',createTeam)
@@ -71,7 +90,12 @@ app.use('/invite',invite)
 app.use('/invited',invited)
 app.use('/accept',accept)
 app.use('/decline',decline)
+app.use('/vote',vote)
+app.use('/voted',voted)
+app.use('/result',pollResult)
+app.use('/endPoll',endPoll)
 
+//server port
 app.listen(3000, ()=>{
     console.log('server started')
 })
