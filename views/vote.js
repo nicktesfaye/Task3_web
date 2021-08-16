40580fe9352573254940bd49cf9f2b3773380c74
nicktesfaye <83650404+nicktesfaye@users.noreply.github.com>
2021-08-16 09:46:47 +0530

    Add files via upload

---
+++ views/vote.js
@@ -0,0 +1,27 @@
+
+const options = document.querySelectorAll("label");
+const submit = document.getElementsByClassName("button")
+for (let i = 0; i < options.length; i++) {
+  options[i].addEventListener("click", ()=>{
+    for (let j = 0; j < options.length; j++) {
+      if(options[j].classList.contains("selected")){
+        options[j].classList.remove("selected");
+      }
+    }
+    options[i].classList.add("selected");
+
+
+    let array = [];
+    for (let l = 0; l < options.length; l++) {
+      if(options[l].classList.contains("selected")){
+        array.push(l);
+      }
+    }
+    if(array.length == 0){
+      for (let m = 0; m < options.length; m++) {
+        options[m].removeAttribute("class");
+      }
+    }
+  });
+}
+
